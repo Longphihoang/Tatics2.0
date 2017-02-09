5ce2019e37cad3f90feed36fce3337800551cc87
Longphihoang <Long.p.hoang1@gmail.com>
2017-02-09 10:46:36 -0800

    Added camera functionality 2/9/17

--- core/src/com/mygdx/game/MyGdxGame.java
+++ core/src/com/mygdx/game/MyGdxGame.java
@@ -5,11 +5,11 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input;
 import com.badlogic.gdx.InputProcessor;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.badlogic.gdx.maps.Map;
 
-import tatics.Account;
 import tatics.Interface;
 import tatics.LevelManager;
 
@@ -18,34 +18,59 @@ public class MyGdxGame extends ApplicationAdapter implements InputProcessor {
 	TextureManager textures;
 	Interface inter;
 	tatics.Map map;
-
+	private OrthographicCamera camera;
+	 float screenWid=0;
+	float screenLen=0;
+	float minLen;
+	float minWid;
+	BitmapFont font;
 	static TextureManager textureManager=new TextureManager();
 	private LevelManager manage;
-
+	private Texture text, text2;
 	public void create () {
+		font = new BitmapFont();
+		screenWid=Gdx.graphics.getWidth();
+		screenLen=Gdx.graphics.getHeight();
 		inter = new Interface();
 		batch = new SpriteBatch();
 		//img = new Texture("badlogic.jpg");
 		textures = new TextureManager();
 		 manage = new LevelManager("testLevel.txt");
-
+		Gdx.input.setInputProcessor(this);
+		inter.loadLevel("testLevel.txt");
+		 text = new Texture(("image.png"));
+		text2 = new Texture("selection.png");
+		camera = new OrthographicCamera(Gdx.graphics.getWidth(),Gdx.graphics.getHeight());
+		camera.translate(Gdx.graphics.getWidth()/2,Gdx.graphics.getHeight()/2);
 	}
 
 	@Override
 	public void render () {
+		inter.update();
+		//camera.position.set(Gdx.graphics.getWidth()/2+inter.selectionX,Gdx.graphics.getHeight()/2+inter.selectionY,0);
+		camera.update();
 		map = manage.getMap();
-		Gdx.gl.glClearColor(1, 0, 0, 1);
+		Gdx.gl.glClearColor(0, 0, 0, 0);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		int len=0;
 
 		batch.begin();
-		for(int i=0;i<map.getLen();i++)
+		batch.draw(text,0,0);
+		//batch.draw(text2,inter.selectionX,inter.selectionY);
+		font.draw(batch,""+inter.selectionX+","+inter.selectionY,inter.selectionX,inter.selectionY);
+		font.draw(batch,""+screenWid+","+screenLen,inter.selectionX,inter.selectionY-50);
+
+		if(inter.selectionX>screenWid)
 		{
-			for(int j=0;j<map.getWidth();j++)
-			{
-				batch.draw(new Texture(map.getTile(j,i).getImage()),j*10,i*10);
-			}
+			screenWid=screenWid+5;
+			camera.position.set(Gdx.graphics.getWidth()/2+5,Gdx.graphics.getHeight()/2,0);
+
 		}
+		if(inter.selectionY>screenLen)
+		{
+			screenLen=screenLen+5;
+		}
+		batch.setProjectionMatrix(camera.combined);
 		//batch.draw(img, 0, 0);
 		batch.end();
 	}
@@ -59,26 +84,44 @@ public class MyGdxGame extends ApplicationAdapter implements InputProcessor {
 	public boolean keyDown(int keycode) {
 		if(keycode== Input.Keys.LEFT)
 		{
-			inter.move("left");
+			inter.input("left");
 		}
 		if(keycode== Input.Keys.DOWN)
 		{
-			inter.move("down");
+			inter.input("down");
 		}
 		if(keycode== Input.Keys.UP)
 		{
-			inter.move("up");
+			inter.input("up");
 		}
 		if(keycode== Input.Keys.RIGHT)
 		{
-			inter.move("right");
+			inter.input("right");
 		}
 		return false;
 	}
 
 	@Override
-	public boolean keyUp(int keycode) {
+	public boolean keyUp(int keycode)
+	{
+		if(keycode== Input.Keys.LEFT)
+		{
+			inter.input("");
+		}
+		if(keycode== Input.Keys.DOWN)
+		{
+			inter.input("");
+		}
+		if(keycode== Input.Keys.UP)
+		{
+			inter.input("");
+		}
+		if(keycode== Input.Keys.RIGHT)
+		{
+			inter.input("");
+		}
 		return false;
+
 	}
 
 	@Override
--- core/src/tatics/Interface.java
+++ core/src/tatics/Interface.java
@@ -3,18 +3,25 @@ package tatics;
 /**
  * Created by long on 2/8/2017.
  */
+/*
+States: 1= map state 0= menu
 
+ */
 public class Interface {
-    int selectionX=0;
-    int selectionY=0;
+    public float selectionX=0;
+    public float selectionY=0;
     Account account;
     Tile tile;
     Map map;
     LevelManager manager;
+    int state=1;
+    String move="";
+
     public Interface()
     {
         Account.getAccount();
     }
+
 
     public void loadLevel(String s)
     {
@@ -40,19 +47,61 @@ public class Interface {
     }
 
 
-    public void move(String s) {
+    public void update(){
+        if(state==1) {
+            if (move.equals("down")) {
+                if (selectionY != 0) {
+                    selectionY = selectionY - 5;
+                }
+            }
 
-        if (s.equals("down")){
-            selectionY=selectionY-1;
+            if (move.equals("up")) {
+                if (selectionY != map.getLen() - 1) {
+                    selectionY = selectionY + 5;
+                }
+            }
+            if (move.equals("right")) {
+                if(selectionX!=map.getWidth()-1) {
+                    selectionX += 5;
+                }
+
+            }
+            if (move.equals("left")) {
+                if(selectionX!=0) {
+                    selectionX += -5;
+                }
+            }
         }
+    }
 
-        if (s.equals("up")){
-            selectionY=selectionY+1;
+    public void input(String s) {
+        if(state==1) {
+            if(s.equals(""))
+            {
+                move="";
+            }
+            if (s.equals("down")) {
+                move="down";
+            }
+
+            if (s.equals("up")) {
+               move="up";
+            }
+            if (s.equals("right")) {
+                move="right";
+
+            }
+            if (s.equals("left")) {
+              move="left";
+            }
         }
-
     }
 
 
+ public int getState()
+ {
+     return state;
+ }
 
 
 }
