8e79ee752782eb7c1ec1b9e76cad69c2fb0c0fa6
Longphihoang <Long.p.hoang1@gmail.com>
2017-02-14 06:51:34 -0800

    Added non movables and revamped tile recon.

---
+++ core/src/com/mygdx/game/StartScreen.java
@@ -0,0 +1,17 @@
+package com.mygdx.game;
+
+import com.badlogic.gdx.graphics.g2d.Sprite;
+
+/**
+ * Created by long on 2/13/2017.
+ */
+
+public class StartScreen extends Sprite {
+    int x;
+    int y;
+    int selection;
+    String newGame= "New Game";
+    String loadLevel = "Load Save";
+    
+
+}
